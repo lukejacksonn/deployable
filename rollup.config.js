7244eea687234f53c377635042dfb081d90234f4
Luke Jackson <lukejacksonn@gmail.com>
2017-06-12 13:06:29 +0100

    Tweaks bundle, static tasks and watch config

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "rollup-plugin-node-resolve"
 import uglify from "rollup-plugin-uglify"
 
 export default {
+  format: 'iife',
   plugins: [
     buble({ jsx: "h" }),
     resolve({ jsnext: true }),
